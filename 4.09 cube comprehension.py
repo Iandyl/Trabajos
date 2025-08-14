32f2d7de091eca01c317ef7555f82ba2c0ebab22
Andres Bernal Valdez <63165905+Iandyl@users.noreply.github.com>
2025-08-14 08:29:58 -0700

    Update 4.09 cube comprehension.py

--- 4.09 cube comprehension.py
+++ 4.09 cube comprehension.py
@@ -1,4 +1,4 @@
-list comprehension to generate a list of the first 10 cubes.
+List comprehension to generate a list of the first 10 cubes.
 
 cubes = [cube**3 for cube in range(1, 11)]
 # Use a list comprehension to generate a list of the first 10 cubes.
